71ddfba675e3ba395c500cad25a78199ffe7d9ff
JoseArevaloz7 <jose.arevaloz@fii-na.com>
2023-07-05 10:10:52 -0600

    add base/templates folder

--- base/views.py
+++ base/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Desing with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms': rooms}
+    return render(request,'base/home.html', context=context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
