f89e8bfa522ff6d0537cbca0523b3db7d8bc1d56
JoseArevaloz7 <jose.arevaloz@fii-na.com>
2023-07-05 13:09:41 -0600

    404 html

--- study/settings.py
+++ study/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=+1@va-@k9e6td7xtqw2v63(mf-^zxpkuo$nh8w8kj-ov)p-@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
