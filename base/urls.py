788a5c248431a8a5ae4e9e517d5663fa22873b58
JoseArevaloz7 <jose.arevaloz@fii-na.com>
2023-07-13 12:13:27 -0600

    add forms and create room form

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'), 
+    path('create-room/<str:pk>/', views.updateRoom, name='create-roomid'), 
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Desing with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python!'},
+#     {'id': 2, 'name': 'Desing with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
 # Create your views here.
@@ -15,13 +16,22 @@ def home(request):
     return render(request=request, template_name='base/home.html', context=context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request=request, template_name='base/room.html', context=context)
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()  
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request=request, template_name='base/room_form.html', context=context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    context = {'form': form}
     return render(request=request, template_name='base/room_form.html', context=context)
